345d0dd43d7f90789d00902bb3e188381899196e
Joshua Tessier <joshpc@gmail.com>
2021-05-17 17:09:15 -0400

    Updates to use statscan data source for crude oil imports

--- data/download_data.sh
+++ data/download_data.sh
@@ -43,7 +43,16 @@ download_file "cer-pentanes-2020.csv" "https://www.cer-rec.gc.ca/open/energy/ene
 download_file "cer-propane-2020.csv" "https://www.cer-rec.gc.ca/open/energy/energyfutures2020/propane-2020.csv"
 
 # Overall Import/Export Data
+# https://apps2.cer-rec.gc.ca/imports-exports/electricity?config=eyJ2aXN1YWxpemF0aW9uU2V0dGluZ3MiOnsiZWxlY3RyaWNpdHnED2Ftb3VudCI6Ik1XLmgiLCJhcnJhbmdlQnkiOiJsb2PFP8QXY3RpdsUzImltcG9ydHNFeMUHIiwic3VidHlwZSI6IsQNxWDENTp7ImPEXHJ5IjpudWxsLCJvcmlnaW5zIjpbXSwiY29udGluZW50xxBkZXN0aW7FcOQAqH0sInByb3ZpbmNlxyHGbzpbXX0sInRpbWVsaW5lxGVzY2FsZUxpbmtlZCI6dHJ1ZSwiZ3JvdXBpbmciOiJ5ZWFyIiwi5QDVxS50YXJ0xAnFGToxOTkwLCJxdWFydGXED30sImVuZMogMjAyzCA0fcQBLOcBBOYBA1bsAWoiOu0BYSwibGFuZ3VhxHwiZeQBSXNob3dFeHBsYekA6%2BYAs29wZW7OGFvMSlRpdGxlyyAiLCJjcnVkZU9pbEnmAZDbIucBq9MibmF0dXJhbEdh3h1MaXF1aWTUJHJlZmluZWRQZXRyb2xldW1Qcm9kdWPTbOYB8mZpZGVudGlhbGl0eU1lbnXsAP5Dzh%2FlAeo%3D
 download_file "CER_imports_exports_data.csv" "https://apps2.cer-rec.gc.ca/imports-exports/data/CER_imports_exports_data.csv"
+
+# https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=2510006301&pickMembers%5B0%5D=1.1&cubeTimeFrame.startMonth=10&cubeTimeFrame.startYear=2016&cubeTimeFrame.endMonth=02&cubeTimeFrame.endYear=2021&referencePeriods=20161001%2C20210201
+
+# Statistics Canada. Table 25-10-0063-01  Supply and disposition of crude oil and equivalent
+# DOI: https://doi.org/10.25318/2510006301-eng
+# "https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=2510006301&pickMembers%5B0%5D=1.1&cubeTimeFrame.startMonth=10&cubeTimeFrame.startYear=2016&cubeTimeFrame.endMonth=02&cubeTimeFrame.endYear=2021&referencePeriods=20161001%2C20210201"
+download_file "statscan_supply_and_dispo_crude_oil_and_equiv.csv" "https://www150.statcan.gc.ca/t1/tbl1/en/dtl!downloadDbLoadingData-nonTraduit.action?pid=2510006301&latestN=0&startDate=20161001&endDate=20210201&csvLocale=en&selectedMembers=%5B%5B1%5D%2C%5B2%2C11%2C14%2C24%5D%2C%5B1%2C2%5D%5D"
+
 
 # Primary Demand
 download_file "cer-primary-energy-demand-2020.csv" "https://www.cer-rec.gc.ca/open/energy/energyfutures2020/primary-energy-demand-2020.csv"
